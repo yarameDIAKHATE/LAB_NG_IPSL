d49bdddcf46d411069ba10162334c099b75ad1b9
Yarame Diakhaté <diakhate.yarame@ugb.edu.sn>
2026-01-22 10:02:28 +0000

    Simulation des produitst

---
+++ src/app/data/produit.data.ts
@@ -0,0 +1,9 @@
+import { Produit } from '../models/produit.models';
+
+export const PRODUITS: Produit[] = [
+  { id: 1, nom: 'Ordinateur Portable', prix: 1200.50, quantite: 10 },
+  { id: 2, nom: 'Souris Sans Fil', prix: 25.99, quantite: 50 },
+  { id: 3, nom: 'Clavier Mécanique', prix: 89.99, quantite: 30 },
+  { id: 4, nom: 'Écran 24"', prix: 199.99, quantite: 15 },
+  { id: 5, nom: 'Casque Audio', prix: 59.99, quantite: 40 }
+];
